5aa84ca65bbdfd88a28c3fd377561d6d4e1e57a4
Zaman Meraj <zaman.iphtech@gmail.com>
2022-02-01 10:34:29 +0530

    Initial Commit

---
+++ IOSChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSChatApp
+//
+//  Created by vivek shrivastwa on 01/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
